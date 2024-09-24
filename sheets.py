86997d02cf5633d8a58b61cde3df2749b129fd04
HarishChandran3304 <harish3304.work@gmail.com>
2024-09-24 23:26:12 +0530

    Set up streamlit secrets (properly this time)

--- sheets.py
+++ sheets.py
@@ -4,11 +4,26 @@ import streamlit as st
 
 
 # Streamlit setup
-credentials = st.secrets["sheets"]["credentials"]
-credentials = dict(credentials)
+credentials = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+    "universe_domain": st.secrets["universe_domain"]
+}
 gc = gspread.service_account_from_dict(credentials)
 
+
+# Local setup
 # gc = gspread.service_account(filename="credentials.json")
+
+
 sh = gc.open("SWE Test")
 worksheet = sh.sheet1
 
